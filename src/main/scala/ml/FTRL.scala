2259b4fcc9a4272ea3d741197040e969e0c6575c
Seventeen17 <xfzjswt@163.com>
2017-07-26 10:58:16 +0800

    aha! Stage completion! - finally output: latest updated dimensions - todo: parameter lamda1/lamda2/alpha/beta/dim/timewindow should receive user input         test

--- src/main/scala/ml/FTRL.scala
+++ src/main/scala/ml/FTRL.scala
@@ -11,18 +11,23 @@ import org.apache.flink.util.Collector
 /**
   * Created by seventeen on 2017/7/13.
   *
+  * Modified by seventeen on 2017/7/26.
+  *    - finally output: latest updated dimensions
+  *    - todo: parameter lamda1/lamda2/alpha/beta/dim/timewindow should receive user input
+  *            test
   */
 object FTRL {
 
   var w = new util.HashMap[Int, Double]()
-//  var wTx = 0.0
+
   val lamda1 = 0.1
   val lamda2 = 0.3
   val alpha = 0.03
   val beta = 1
   val dim = 100
-  val z: Array[Double] = new Array[Double](dim)
-  val n: Array[Double] = new Array[Double](dim)
+
+  var z: Array[Double] = new Array[Double](dim)
+  var n: Array[Double] = new Array[Double](dim)
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -33,7 +38,6 @@ object FTRL {
     val trainingData = env.socketTextStream(hostname, port, '\n')
 
     // input stream: label feature_index1: value1, feature_index2: value2, ...,
-
     val data: DataStream[Data] = trainingData
         .map(s => {
           var wTx = 0.0
@@ -48,11 +52,11 @@ object FTRL {
         x._2.split(',').map((x._1, _, x._3))
       }).map(x => {
       val splits = x._2.split(':')
-      Data(x._1, splits(1).toDouble, splits(2).toDouble, x._3)
+      Data(splits(0).toInt, splits(1).toDouble, x._1, x._3)
     })
 
       val model: DataStream[Params] = data
-      .filter(x => x.index < dim && x.index >= 0)  // index小于0或大于维度的过滤
+      .filter(x => x.index < dim && x.index >= 0)
       .keyBy(_.index)
       .timeWindow(Time.seconds(10))
       .apply{(
@@ -65,49 +69,33 @@ object FTRL {
     model.print().setParallelism(1)
     env.execute("model train")
 
-
-/*    val data: DataStream[Data] = trainingData
-      .map(s => {
-      val splits = s.split(",")
-      Data(splits(0).toInt, splits(1).toDouble, splits(2).toDouble, splits(3).toDouble)
-    })*/
-
-/*    val model: DataStream[Params] = data
-        .filter(x => x.index < dim && x.index >= 0)  // index小于0或大于维度的过滤
-        .keyBy(_.index)
-      .timeWindow(Time.seconds(10))
-        .apply{(
-          key: Int,
-          window: TimeWindow,
-          events: Iterable[Data],
-          out: Collector[Params]) =>
-          out.collect(buildPartialModel(key, events))
-        }
-    model.print().setParallelism(1)
-    env.execute("model train")*/
-
   }
 
 
   def buildPartialModel(key: Int, value: Iterable[Data]): Params = {
-    var postW = w.get(key)
-    var prob = 0
+
+    val iniW = w.get(key)
+    var postW = iniW
+    var prob = 0.0
     var zi = z(key)
     var ni = n(key)
-    value.map{
-      e => {
-        (postW, prob) = predict(zi, ni, key, e, postW)  // 串行 or 并行？
-        (zi, ni) = update(key, prob, postW, e, zi, ni)
-        0
-      }
+
+    for (e <- value){
+      val x = predict(zi, ni, key, e, postW, iniW)
+      postW = x._1
+      prob = x._2
+      val y = update(key, prob, postW, e, zi, ni)
+      zi = y._1
+      ni = y._2
     }
+
     w.put(key, postW)
     z(key) = zi
     n(key) = ni
     Params(key, postW)
   }
 
-  def predict(zi: Double, ni: Double, key: Int, e: Data, postW: Double): (Double, Double) = {
+  def predict(zi: Double, ni: Double, key: Int, e: Data, postW: Double, iniW: Double): (Double, Double) = {
     var post = postW
     val sgn = {
       if (zi < 0){
@@ -123,13 +111,13 @@ object FTRL {
     else{
       post = (sgn * lamda1 - zi) / (lamda2 + (beta + Math.sqrt(ni)) / alpha)
     }
-    val wTx  = e.wTx - w.get(key) * e.value + post * e.value
+    val wTx  = e.wTx - iniW * e.value + post * e.value
     (post, 1 / (1 + Math.exp(-Math.max(Math.min(wTx, 35), -35))))
   }
 
   def update(key: Int, prob: Double, w:Double, e: Data, zi:Double, ni:Double): (Double, Double) = {
-    var zi = 0
-    var ni = 0
+    var zii = zi
+    var nii = ni
     val ans = {
       if (e.label > 0){
         1
@@ -139,16 +127,12 @@ object FTRL {
       }
 
     }
-      val g = (prob - ans) * e.value
-      val sigma = ( Math.sqrt(n(key) + g * g) - Math.sqrt(n(key))) / alpha
-      zi += g - sigma * w
-      ni += g*g
-    (zi, ni)
+      val g: Double = (prob - ans) * e.value
+      val sigma: Double = ( Math.sqrt(ni + g * g) - Math.sqrt(ni)) / alpha
+      zii = zii + g - sigma * w
+      nii = nii + g*g
+    (zii, nii)
   }
-
-/*  def partialGradient(x: Double, y : Double): Double = {
-    y - x
-  }*/
 
   case class Data(index: Int, value:Double, label: Double, wTx: Double)
 
